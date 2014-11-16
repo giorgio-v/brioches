941ae07637ea35e9d38e50399080266fe0919c4c
giorgio-v <giorgio@refactor.it>
2014-11-16 07:51:35 +0100

    Project dependencies
    - Added `org.clojure/tools.namespace` and
      `org.clojure/math.combinatorics`.
    
    Signed-off-by: giorgio-v <giorgio@refactor.it>

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [prismatic/schema "0.3.2"]]
+                 [prismatic/schema "0.3.2"]
+                 [org.clojure/math.combinatorics "0.0.8"]
+                 [org.clojure/tools.namespace "0.2.7"]]
   :profiles {:dev
              {:dependencies [[org.clojure/test.check "0.5.9"]]
               :source-paths ["dev"]}})
